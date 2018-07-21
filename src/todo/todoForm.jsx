369ffa1b754278a331b086dd4172c353f8082110
Lucas de Oliveira Queiroz <lucasoliveiraoueiroz@gmail.com>
2018-07-21 13:30:15 -0300

    Commit inicial do projeto funcional sem implementação do redux

---
+++ src/todo/todoForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }
+        else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (
+        <div role='form' className='todoForm'>
+            <Grid cols='12 9 10'>
+                <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+                    value={props.description}
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                />
+            </Grid>
+
+            <Grid cols='12 3 2'>
+                <IconButton
+                    onClick={props.handleAdd}
+                    style='primary'
+                    icon='plus'
+                />
+                <IconButton
+                    onClick={props.handleSearch}
+                    style='info'
+                    icon='search'
+                />
+                <IconButton
+                    onClick={props.handleClear}
+                    style='default'
+                    icon='close'
+                />
+            </Grid>
+        </div >
+    )
+}
